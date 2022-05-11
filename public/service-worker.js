532514555c76b70a2abfadb317ee81c14e2e81d0
Echeta Okeke <echeta@rogers.com>
2022-05-11 16:24:44 -0400

    corrected typo with cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -10,7 +10,7 @@ const FILES_TO_CACHE = [
     "icons/icon-96x96.png",
     "icons/icon-128x128.png",
     "icons/icon-144x144.png",
-    "icons/icon-1521522.png",
+    "icons/icon-152x152.png",
     "icons/icon-192x192.png",
     "icons/icon-384x384.png",
     "icons/icon-512x512.png"
