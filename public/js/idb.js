4a663efa01cf38cab0f7e58bfe8f7f5f98ce7d6e
Echeta Okeke <echeta@rogers.com>
2022-05-11 16:45:57 -0400

    corrected api fetch command

--- public/js/idb.js
+++ public/js/idb.js
@@ -57,7 +57,7 @@ function saveRecord(record) {
     getAll.onsuccess = function() {
         // if there was data in indexedDb's store, let's send it to the api server
         if (getAll.result.length > 0) {
-        fetch('/api/budget', {
+        fetch('/api/transaction', {
             method: 'POST',
             body: JSON.stringify(getAll.result),
             headers: {
