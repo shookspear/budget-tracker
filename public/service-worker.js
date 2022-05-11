9fc607329b44d4303672aa08879690fc1cf77e9e
Echeta Okeke <echeta@rogers.com>
2022-05-11 16:20:01 -0400

    updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,6 +16,23 @@ const FILES_TO_CACHE = [
     "icons/icon-512x512.png"
   ];
 
+
+  self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request)
+        }
+
+      })
+    )
+  })
+
   self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
@@ -45,20 +62,3 @@ const FILES_TO_CACHE = [
     );
   });
 
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-  
-        // You can omit if/else for console.log & put one line below like this too.
-        // return request || fetch(e.request)
-      })
-    )
-  })
