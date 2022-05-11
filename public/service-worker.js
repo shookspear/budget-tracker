a1386bbc15cbe3bafc919a6b6e96563cd1b19b27
Echeta Okeke <echeta@rogers.com>
2022-05-11 17:13:39 -0400

    corrcted js syntax

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,46 +2,33 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-    "index.html",
-    "css/styles.css",
-    "js/idb.js",
-    "js/index.js",
-    "icons/icon-72x72.png",
-    "icons/icon-96x96.png",
-    "icons/icon-128x128.png",
-    "icons/icon-144x144.png",
-    "icons/icon-152x152.png",
-    "icons/icon-192x192.png",
-    "icons/icon-384x384.png",
-    "icons/icon-512x512.png"
+    "/",
+    "/index.html",
+    "/css/styles.css",
+    "/js/idb.js",
+    "/js/index.js",
+    "/icons/icon-72x72.png",
+    "/icons/icon-96x96.png",
+    "/icons/icon-128x128.png",
+    "/icons/icon-144x144.png",
+    "/icons/icon-152x152.png",
+    "/icons/icon-192x192.png",
+    "/icons/icon-384x384.png",
+    "/icons/icon-512x512.png"
   ];
 
-
-  self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { // if cache is available, respond with cache
-          console.log('responding with cache : ' + e.request.url)
-          return request
-        } else {       // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + e.request.url)
-          return fetch(e.request)
-        }
-
-      })
-    )
-  })
-
+  // Install Service Worker
   self.addEventListener('install', function (e) {
     e.waitUntil(
       caches.open(CACHE_NAME).then(function (cache) {
-        console.log('installing cache : ' + CACHE_NAME)
-        return cache.addAll(FILES_TO_CACHE)
+        console.log('installing cache : ' + CACHE_NAME);
+        return cache.addAll(FILES_TO_CACHE);
       })
-    )
-  })
+    );
+  });
 
+
+  // Activate Service Worker
   self.addEventListener('activate', function(e) {
     e.waitUntil(
       caches.keys().then(function(keyList) {
@@ -61,4 +48,26 @@ const FILES_TO_CACHE = [
       })
     );
   });
+
+
+  // Intercept Fecth Requests
+  self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+      caches.match(e.request).then(function (request) {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + e.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + e.request.url)
+          return fetch(e.request);
+        }
+
+      })
+    );
+  });
+
+
+
+
 
